ff9813b4fcf6affb2daef34436adae62906450a5
kunal <ks1140175@gmail.com>
2025-12-05 17:43:57 +0530

    profile selection complete

--- src/lib/SupabaseAuthClient.ts
+++ src/lib/SupabaseAuthClient.ts
@@ -1,5 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from "@supabase/ssr"
 
-const supabaseUrl = 'https://vdxmxeprvqiwbuimjmzh.supabase.co'//process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkeG14ZXBydnFpd2J1aW1qbXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2OTQ0MzcsImV4cCI6MjA2MTI3MDQzN30.0BH07GIJkUH4yBgAc0E4V6hZJ_Mh-jLxtigER2Ea4y4'    //process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!) 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables")
+}
+
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
