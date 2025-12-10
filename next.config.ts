c2c11dd6404a5402ef8f67e1047fc060156922a4
kunal <ks1140175@gmail.com>
2025-12-10 13:39:44 +0530

    resolve sanity issue

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
 import type { NextConfig } from "next";
 
-/**
- * Add cdn.sanity.io under images.domains to allow next/image to load Sanity images
- */
 const nextConfig: NextConfig = {
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'cdn.sanity.io',
-    },
-  ],
-}
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+    unoptimized: true, // 👈 THIS FIXES THE ERROR
+  },
 };
-
 
 export default nextConfig;
